dc21c3896c52c67f7dc4d151c2fc30f37306ff12
Tommy Speigner <tspeigner@gmail.com>
2018-11-05 17:09:26 -0800

    fixing license file path

--- site/profile/manifests/app/discovery.pp
+++ site/profile/manifests/app/discovery.pp
@@ -1,7 +1,7 @@
 class profile::app::discovery (
   $pd_url     = 'https://goo.gl/e1Jsi1', #shortened URL of Discovery download
   $license_file = '/root/puppet_discovery/discovery.license.json',
-  $pd_license = "puppet:///modules/profile/app/$license_file", #license file on master
+  $pd_license = "puppet:///modules/profile/app/discovery.license.json", #license file on master
 ) {
   # Baseline and install Docker
   require ::profile::platform::baseline
