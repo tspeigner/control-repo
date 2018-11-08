f3a35e8eaf8122d52777057bdddb51e0b8cd8047
Tommy Speigner <tspeigner@gmail.com>
2018-11-07 20:48:34 -0800

    changing cd4pe host ip query

--- site/profile/manifests/app/cd4pe_buildserver/hosts.pp
+++ site/profile/manifests/app/cd4pe_buildserver/hosts.pp
@@ -2,11 +2,10 @@
 class profile::app::cd4pe_buildserver::hosts {
 
   $master_server = $::settings::server
-
-  $cd4pe_query = "facts[value]{ name in ['ipaddress_enp0s8',  'ipaddress_eth0']
-    and certname in resources[certname] { certname = 'puppet.ts-aws.tsedemos.com' } }"
-
-  $cd4pe_ip = puppetdb_query($cd4pe_query)
+  
+  $cd4pe_query        = "inventory[facts] { facts.trusted.certname ~ 'cd4pe' }"
+  $response           = puppetdb_query($cd4pe_query)
+  $cd4pe_ip           = $response[0]['facts']['ipaddress']
 
   host { 'cd4pe.pdx.puppet.vm':
     ensure       => 'present',
