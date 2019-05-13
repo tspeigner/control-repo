b48a7bcea4142de3d7141befe997ff65087b25cf
Tommy Speigner <tspeigner@gmail.com>
2019-05-13 13:14:45 -0700

    adding to baseline

--- site-modules/profile/manifests/platform/baseline.pp
+++ site-modules/profile/manifests/platform/baseline.pp
@@ -18,14 +18,25 @@ class profile::platform::baseline (
     include ::profile::app::sensu::client
   }
 
-  # OS Specific
-  case $::kernel {
-    'windows': {
-      include ::profile::platform::baseline::windows
-    }
-    default: {
-      fail('Unsupported operating system!')
-    }
+  $plat = $facts.get('os.family') ? {
+    #If os.family == 'windows' then $plat = 'windows', default is 'linux'
+    /windows/ => 'windows',
+    default   => 'linux'
   }
 
+  $env = $facts.get('ipaddress') ? {
+    # If a node has an IP in the range 10.0.24.253-255 it's in the Dev VPC
+    /^10\.0\.24\.(2(5[3-5]))$/   => 'dev',
+    # If a node has an IP in the range 10.0.24.243-245 it's in the UAT VPC
+    /^10\.0\.24\.(2(4[3-5]))$/   => 'uat',
+    # If a node has an IP in the range 10.0.24.233-235 it's in the Prod VPC
+    /^10\.0\.24\.(2(3[3-5]))$/   => 'prod',
+    default => fail('Environment does not exist'),
+  }
+# If env = dev and plat = linux == profile::platform::baseline::linux::dev
+# This is interpolated in the 'include' below
+notify { 'plat and env':
+  message => "${plat} and ${env}"
+}
+include "profile::platform::baseline::${plat}::${env}"
 }
